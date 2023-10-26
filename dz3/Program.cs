0aff3c1657d39f2d0686b622bd0ee34549ccdb85
shiirik <mr.pakman04@gmail.com>
2023-10-26 09:52:59 +0600

    Добавьте файлы проекта.

---
+++ dz3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string input = "Это строка с некоторыми словами, которые содержат букву 'a'.";
+
+        // Используем регулярное выражение для поиска слов с буквой 'a'
+        string pattern = @"\b\w*?a\w*\b";
+        string result = Regex.Replace(input, pattern, "");
+
+        Console.WriteLine("Исходная строка: " + input);
+        Console.WriteLine("Строка после удаления слов с 'a': " + result);
+    }
+}
+
